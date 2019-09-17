8ada7beccb13ce48c776efde7a6cc4628aac8378
Elisabeth Jessica Valencia <elisabethtsy29@gmail.com>
2019-09-17 14:21:08 +0700

    Initial Commit

---
+++ projectNC2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  projectNC2
+//
+//  Created by Elisabeth Jessica Valencia on 17/09/19.
+//  Copyright © 2019 Elisabeth Jessica Valencia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
